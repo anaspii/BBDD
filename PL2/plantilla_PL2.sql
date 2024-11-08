0cb5789abd91e18ab934935bb0710c86191a4561
anaspii <anastasiia.petrova@edu.uah.es>
2024-11-08 09:20:17 +0100

    Consultas

--- PL2/plantilla_PL2.sql
+++ PL2/plantilla_PL2.sql
@@ -67,11 +67,14 @@ CREATE TABLE Edita_temp (
 -- Tabla Tiene (relación Usuario - Ediciones)
 CREATE TABLE Tiene_temp (
     Nombre_usuario VARCHAR(255),
+    Titulo_disco VARCHAR(100),
+    Año_disco INT,
     Formato_edicion VARCHAR(100),
     Año_edicion INT,
     Pais_edicion VARCHAR(100),
     PRIMARY KEY (Nombre_usuario, Formato_edicion, Año_edicion, Pais),
     FOREIGN KEY (Nombre_usuario) REFERENCES Usuario(Nombre_usuario),
+    FOREIGN KEY (Titulo_disco, Año_disco) REFERENCES Disco(Titulo, Año_publicacion),
     FOREIGN KEY (Formato_edicion, Año_edicion, Pais_edicion) REFERENCES Ediciones(Formato, Año_edicion, Pais)
 );
 
@@ -100,7 +103,7 @@ SET search_path='nombre del esquema o esquemas utilizados';
 
 \copy Desea_temp(Nombre_usuario, Titulo_disco, Año_disco) FROM 'usuario_desea_disco.csv' WITH CSV HEADER;
 
-\copy Tiene_temp(Nombre_usuario, Formato_edicion, Año_edicion, Pais_edicion) FROM 'usuario_tiene_edicion.csv' WITH CSV HEADER;
+\copy Tiene_temp(Nombre_usuario, Titulo_disco, Año_disco, Formato_edicion, Año_edicion, Pais_edicion) FROM 'usuario_tiene_edicion.csv' WITH CSV HEADER;
 
 
 \echo 'Cargando datos'
@@ -142,14 +145,131 @@ SELECT Nombre_usuario, Titulo_disco, Año_disco
 FROM Desea_temp
 WHERE Nombre_usuario IS NOT NULL;
 
-INSERT INTO Tiene (Nombre_usuario, Formato_edicion, Año_edicion, Pais_edicion)
-SELECT Nombre_usuario, Formato_edicion, Año_edicion, Pais_edicion
+INSERT INTO Tiene (Nombre_usuario, Titulo_disco, Año_disco, Formato_edicion, Año_edicion, Pais_edicion)
+SELECT Nombre_usuario, Titulo_disco, Año_disco, Formato_edicion, Año_edicion, Pais_edicion
 FROM Tiene_temp
 WHERE Nombre_usuario IS NOT NULL;
 
-\echo Consulta 1: texto de la consulta
+\echo Consulta 1: 'Mostrar los discos que tengan más de 5 canciones.'
 
-\echo Consulta n:
+SELECT Titulo_disco, Año_disco
+FROM Canciones_temp
+GROUP BY Titulo_disco, Año_disco
+HAVING COUNT(Titulo) > 5;
+
+
+\echo Consulta 2: 'Mostrar los vinilos que tiene el usuario Juan García Gómez junto con el título del
+disco, y el país y año de edición'
+
+SELECT Ediciones.Titulo_disco, Ediciones.Año_disco, Ediciones.Pais, Ediciones.Año_edicion
+FROM Tiene
+JOIN Ediciones ON Tiene.Formato_edicion = Ediciones.Formato AND Tiene.Año_edicion = Ediciones.Año_edicion AND Tiene.Pais_edicion = Ediciones.Pais
+JOIN Usuario ON Tiene.Nombre_usuario = Usuario.Nombre_usuario
+WHERE Usuario.Nombre = 'Juan García Gómez';
+
+
+\echo Consulta 3: 'Disco con mayor duración de la colección.'
+
+SELECT Titulo_disco, Año_disco, SUM(EXTRACT(EPOCH FROM Duracion)) AS Duracion_total
+FROM Canciones
+GROUP BY Titulo_disco, Año_disco
+ORDER BY Duracion_total DESC
+LIMIT 1;
+
+
+\echo Consulta 4: 'De los discos que tiene en su lista de deseos el usuario Juan García Gómez,
+indicar el nombre de los grupos musicales que los interpretan.'
+
+SELECT DISTINCT Grupo.Nombre
+FROM Desea
+JOIN Edita ON Desea.Titulo_disco = Edita.Titulo_disco AND Desea.Año_disco = Edita.Año_disco
+JOIN Grupo ON Edita.Nombre_grupo = Grupo.Nombre
+JOIN Usuario ON Desea.Nombre_usuario = Usuario.Nombre_usuario
+WHERE Usuario.Nombre = 'Juan García Gómez';
+
+
+\echo Consulta 5: 'Mostrar los discos publicados entre 1970 y 1972 junto con sus ediciones
+ordenados por el año de publicación'
+
+SELECT Disco.Titulo, Disco.Año_publicacion, Ediciones.Formato, Ediciones.Año_edicion, Ediciones.Pais
+FROM Disco
+JOIN Ediciones ON Disco.Titulo = Ediciones.Titulo_disco AND Disco.Año_publicacion = Ediciones.Año_disco
+WHERE Disco.Año_publicacion BETWEEN 1970 AND 1972
+ORDER BY Disco.Año_publicacion;
+
+
+\echo Consulta 6: 'Listar el nombre de todos los grupos que han publicado discos del género
+‘Electronic’.'
+
+SELECT DISTINCT Grupo.Nombre
+FROM Disco
+JOIN Edita ON Disco.Titulo = Edita.Titulo_disco AND Disco.Año_publicacion = Edita.Año_disco
+JOIN Grupo ON Edita.Nombre_grupo = Grupo.Nombre
+WHERE Disco.Géneros LIKE 'Electronic';
+
+
+\echo Consulta 7: 'Lista de discos con la duración total del mismo, editados antes del año 2000'
+
+SELECT Disco.Titulo, Disco.Año_publicacion, SUM(EXTRACT(EPOCH FROM Canciones.Duracion)) AS Duracion_total
+FROM Disco
+JOIN Canciones ON Disco.Titulo = Canciones.Titulo_disco AND Disco.Año_publicacion = Canciones.Año_disco
+WHERE Disco.Año_publicacion < 2000
+GROUP BY Disco.Titulo, Disco.Año_publicacion;
+
+
+\echo Consulta 8: 'Lista de ediciones de discos deseados por el usuario Lorena Sáez Pérez que tiene
+el usuario Juan García Gómez.'
+
+SELECT Desea.Titulo_disco, Desea.Año_disco, Ediciones.Formato, Ediciones.Año_edicion, Ediciones.Pais
+FROM Desea
+JOIN Tiene ON Desea.Titulo_disco = Tiene.Titulo_disco AND Desea.Año_disco = Tiene.Año_disco
+JOIN Usuario AS UsuarioDesea ON Desea.Nombre_usuario = UsuarioDesea.Nombre_usuario
+JOIN Usuario AS UsuarioTiene ON Tiene.Nombre_usuario = UsuarioTiene.Nombre_usuario
+JOIN Ediciones ON Desea.Titulo_disco = Ediciones.Titulo_disco AND Desea.Año_disco = Ediciones.Año_disco
+WHERE UsuarioDesea.Nombre = 'Lorena Sáez Pérez' AND UsuarioTiene.Nombre = 'Juan García Gómez';
+
+
+\echo Consulta 9: 'Lista todas las ediciones de los discos que tiene el usuario Gómez García en un
+estado NM o M.'
+
+SELECT Ediciones.Titulo_disco, Ediciones.Año_disco, Ediciones.Formato, Ediciones.Año_edicion, Ediciones.Pais
+FROM Tiene
+JOIN Ediciones ON Tiene.Formato_edicion = Ediciones.Formato AND Tiene.Año_edicion = Ediciones.Año_edicion AND Tiene.Pais_edicion = Ediciones.Pais
+JOIN Usuario ON Tiene.Nombre_usuario = Usuario.Nombre_usuario
+WHERE Usuario.Nombre = 'Gómez García' AND (Ediciones.Estado = 'NM' OR Ediciones.Estado = 'M');
+
+
+\echo Consulta 10: 'Listar todos los usuarios junto al número de ediciones que tiene de todos los discos
+junto al año de lanzamiento de su disco más antiguo, el año de lanzamiento de su
+disco más nuevo, y el año medio de todos sus discos de su colección.'
+
+SELECT Usuario.Nombre, COUNT(Ediciones.Titulo_disco) AS num_ediciones, MIN(Disco.Año_publicacion) AS año_min, MAX(Disco.Año_publicacion) AS año_max, AVG(Disco.Año_publicacion) AS año_medio
+FROM Tiene
+JOIN Ediciones ON Tiene.Formato_edicion = Ediciones.Formato AND Tiene.Año_edicion = Ediciones.Año_edicion AND Tiene.Pais_edicion = Ediciones.Pais
+JOIN Disco ON Ediciones.Titulo_disco = Disco.Titulo AND Ediciones.Año_disco = Disco.Año_publicacion
+JOIN Usuario ON Tiene.Nombre_usuario = Usuario.Nombre_usuario
+GROUP BY Usuario.Nombre;
+
+
+\echo Consulta 11: 'Listar el nombre de los grupos que tienen más de 5 ediciones de sus discos en la
+base de datos.'
+
+SELECT Grupo.Nombre
+FROM Edita
+JOIN Grupo ON Edita.Nombre_grupo = Grupo.Nombre
+GROUP BY Grupo.Nombre
+HAVING COUNT(Edita.Titulo_disco) > 5;
+
+
+\echo Consulta 12: '. Lista el usuario que más discos, contando todas sus ediciones tiene en la base de
+datos.'
+
+SELECT Usuario.Nombre
+FROM Tiene
+JOIN Usuario ON Tiene.Nombre_usuario = Usuario.Nombre_usuario
+GROUP BY Usuario.Nombre
+ORDER BY COUNT(Tiene.Titulo_disco) DESC
+LIMIT 1;
 
 
 ROLLBACK;                       -- importante! permite correr el script multiples veces...p
